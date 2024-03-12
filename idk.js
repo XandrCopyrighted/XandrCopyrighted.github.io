b678ec6da03d66b4912f592a51a9133fbfe82be0
XDR <111686716+XandrCopyrighted@users.noreply.github.com>
2024-03-11 19:53:12 -0500

    GitHub page is deprecated

--- idk.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const $body = document.body;
-  const loadHandler = () => setTimeout(() => {
-    $body.classList.remove('is-loading', 'is-playing');
-    $body.classList.add('is-playing', 'is-ready');
-  }, 100);
-
-  window.addEventListener('load', loadHandler);
-});
